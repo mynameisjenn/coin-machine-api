217fcce6a7af55951f02cdc7bdcb06502cdc9a36
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-03-11 14:09:53 -0500

    created user and session controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get '/total' => 'api/coins#total'
+  # get '/total' => 'api/coins#total'
+
   namespace :api do
   	resources :coins  
   	resources :transactions
-
+  	post "/users" => "users#create"
+  	post "/sessions" => "sessions#create"
   end
 end

cb7aebc1c7c5b45154e43d346572e48328307610
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-03-13 17:21:13 -0500

    reseeded a bit of data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-coin = Coin.new(name: "quarter", value: 0.25)
-coin.save
+# coin = Coin.new(name: "quarter", value: 0.25)
+# coin.save
 
-coin = Coin.new(name: "dime", value: 0.10)
-coin.save
+# coin = Coin.new(name: "dime", value: 0.10)
+# coin.save
 
-coin = Coin.new(name: "nickel", value: 0.05)
-coin.save
+# coin = Coin.new(name: "nickel", value: 0.05)
+# coin.save
 
-coin = Coin.new(name: "penny", value: 0.01)
-coin.save
+# coin = Coin.new(name: "penny", value: 0.01)
+# coin.save
+
+transaction = Transaction.new(coin_id: 19, deposited: true, user_id: 1)
+transaction.save
 
